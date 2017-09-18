a4f9d5cce254f039f89a4831accbb82d72e79522
Olaydub <owaller@uw.edu>
2017-09-17 21:26:59 -0700

    uploading my p5.js sketch

---
+++ waller_randomDots/sketchRandomDots.js
@@ -0,0 +1,27 @@
+var spot = {
+x: 100,
+y: 50,
+}
+
+var col = {
+    r: 0,
+    g: 255,
+    b: 0,
+}
+
+function setup() {
+    createCanvas(1240, 1080);
+    background(0);
+}
+
+function draw() {
+col.r = random(0, 255);
+col.g = random(0, 255);
+col.b = random(0, 255);
+spot.x = random(0, width);
+spot.y = random(0, height);
+noStroke();
+fill(col.r, col.g, col.b, 80);
+ellipse(spot.x, spot.y, 100, 100);
+
+}
